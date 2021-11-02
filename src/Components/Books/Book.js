7c817c1883b97023f745e07afcc472700868d032
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-02 14:37:51 +0100

    adding and removing books func using redux store/action/reducer

--- src/Components/Books/Book.js
+++ src/Components/Books/Book.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
-const books = [
-  { id: 1, name: 'Meditations', author: 'Marcus Aurelius' },
-  { id: 1, name: 'Crime and punishement', author: 'Dostoevsky' },
-];
+const Book = () => {
+  const books = useSelector((state) => state.booksReducer);
 
-const Book = () => books.map((book) => (
-  <li key={book.id}>
-    <span>
-      {' '}
-      {book.name}
-    </span>
-    <span>
-      {' '}
-      {book.author}
-    </span>
-    <button type="button">remove</button>
-  </li>
-));
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = (book) => {
+    dispatch(removeBook(book));
+  };
+
+  return books.map((book) => (
+    <li key={book.id}>
+      <span>
+        {' '}
+        {book.title}
+      </span>
+      <span>
+        {' '}
+        {book.author}
+      </span>
+      <button type="button" onClick={() => handleRemoveBook(book)}>remove</button>
+    </li>
+  ));
+};
 
 export default Book;
