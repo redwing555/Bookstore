5f5963a182666050bd22d7b032e855e3f02b869a
Hichami Houssam <hichami.houssam@gmail.com>
2021-11-03 14:25:50 +0100

    creating api func , for adding, deleting, retriving books from the created api bookstore

--- src/Components/Books/Book.js
+++ src/Components/Books/Book.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/books';
+import { removeBook, addBook } from '../../redux/books/books';
+import { fetchBookstoreData, removeBookApi } from '../../api/api';
 
 const Book = () => {
   const books = useSelector((state) => state.booksReducer);
@@ -8,10 +9,30 @@ const Book = () => {
   const dispatch = useDispatch();
 
   const handleRemoveBook = (book) => {
+    removeBookApi(book.id);
     dispatch(removeBook(book));
   };
 
+  const getBooksFromApi = async () => {
+    const bookObject = await fetchBookstoreData();
+    const bookData = Object.keys(bookObject).map((_, index) => ({
+      id: Object.keys(bookObject)[index],
+      author: Object.values(bookObject)[index][0].category,
+      title: Object.values(bookObject)[index][0].title,
+
+    }));
+
+    bookData.forEach((book) => {
+      dispatch(addBook(book));
+    });
+  };
+
+  useEffect(() => {
+    getBooksFromApi();
+  }, []);
+
   return books.map((book) => (
+
     <li key={book.id}>
       <span>
         {' '}
